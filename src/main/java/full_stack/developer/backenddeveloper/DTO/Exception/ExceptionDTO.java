616097fd2bff3d38c063ba6eb0f1a70b360a408c
Rafee Ahmed <abdulrafee0078@gmail.com>
2026-08-03 17:12:37 +0530

    new code files

---
+++ src/main/java/full_stack/developer/backenddeveloper/DTO/Exception/ExceptionDTO.java
@@ -0,0 +1,63 @@
+package full_stack.developer.backenddeveloper.DTO.Exception;
+
+import java.time.LocalDateTime;
+
+public class ExceptionDTO {
+
+    private LocalDateTime timestamp;
+    private int statusCode;
+    private String error;
+    private String message;
+    private String path;
+
+    public ExceptionDTO(LocalDateTime timestamp, int statusCode, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.statusCode = statusCode;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public ExceptionDTO() {
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
