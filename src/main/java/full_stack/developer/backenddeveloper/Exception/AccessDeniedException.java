90018a5fb6fe9ebe70d59c0ff8af1d1e8879d84a
Rafee Ahmed <abdulrafee0078@gmail.com>
2026-08-05 22:46:53 +0530

    new code files

---
+++ src/main/java/full_stack/developer/backenddeveloper/Exception/AccessDeniedException.java
@@ -0,0 +1,8 @@
+package full_stack.developer.backenddeveloper.Exception;
+
+public class AccessDeniedException extends RuntimeException{
+
+    public AccessDeniedException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/full_stack/developer/backenddeveloper/Exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package full_stack.developer.backenddeveloper.Exception;
+
+public class AuthenticationException extends RuntimeException{
+    public AuthenticationException(String message){
+        super(message);
+    }
+}
