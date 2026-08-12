32367796807d3c87dd6da8ac201f0a65c3a485f1
Rafee Ahmed <abdulrafee0078@gmail.com>
2026-08-12 19:52:00 +0530

    new code files

--- src/main/java/full_stack/developer/backenddeveloper/Entity/Role.java
+++ src/main/java/full_stack/developer/backenddeveloper/Entity/Role.java
@@ -1,9 +1,20 @@
 package full_stack.developer.backenddeveloper.Entity;
 
 
+import java.util.Optional;
+
 public enum Role {
-    
-     USER,
-    ADMIN
-  
+
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    private final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/full_stack/developer/backenddeveloper/Entity/Signup.java
+++ src/main/java/full_stack/developer/backenddeveloper/Entity/Signup.java
@@ -2,9 +2,15 @@ package full_stack.developer.backenddeveloper.Entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,16 +20,16 @@ import java.time.Instant;
 @Table(
         name = "Signup_Authentication"
 )
-public class Signup {
+public class Signup implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Signup_id")
     private Long id;
     @Column(name = "Signup_email" , unique = true, length = 300, nullable = false)
     private String email;
     @Column(name = "Signup_name", length = 200)
     private String name;
-    @Column(name = "Signup_Password" , unique = true, nullable = false)
+    @Column(name = "Signup_Password" ,  nullable = false)
     private String password;
     private String image;
     private boolean enable = true;
@@ -33,7 +39,7 @@ public class Signup {
     private  Provider provider = Provider.LOCAL;
 
     @Enumerated(EnumType.STRING)
-    private Role Roles;
+    private Role role = Role.USER;
 
     @PrePersist
     protected void OnCreate() {
@@ -47,4 +53,33 @@ public class Signup {
     }
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enable;
+    }
 }
